b73058c350a1bd79d9973e49ba156ac01c33e182
SPR <sebaspr16@gmail.com>
2021-04-21 09:52:22 -0500

    Initial commit -spring boot project created

---
+++ src/main/java/com/mxtc/carrentalproject/CarrentalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mxtc.carrentalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrentalprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mxtc/carrentalproject/CarrentalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxtc.carrentalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrentalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
